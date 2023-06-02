996f06db4f6ad1b65e3c4ee4722422ad054e8ee4
carmencmattos <carmencmattos@gmail.com>
2023-06-02 14:20:16 -0300

    feature: add header

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -7,6 +7,12 @@ export default function Cardapio () {
             <nav className={styles.menu}>
                 <Logo />
             </nav>
+            <header className={styles.header} >
+                <div className={styles.header__text}>
+                    A casa da massa
+                </div>
+
+            </header>
         </main>
     )
 }
