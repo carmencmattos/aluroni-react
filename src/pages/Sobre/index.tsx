305a929a89156df11d9363599285ea333b079e6b
carmencmattos <carmencmattos@gmail.com>
2023-06-10 11:32:43 -0300

    feature: add footer

--- src/pages/Sobre/index.tsx
+++ src/pages/Sobre/index.tsx
@@ -21,12 +21,12 @@ export default function Sobre() {
             intensa!
           </p>
           <p>
-            Também possuímos uma cardápio de carnes com muitas opções de acordo
+            Também possuímos um cardápio de carnes com muitas opções de acordo
             com o seu gosto!
           </p>
           <p>
             Para acompanhar as massas italianas, Aluroni possui uma reserva de
-            vinhos especiais, que harmonizam perfeitamente com o seu parto, seja
+            vinhos especiais, que harmonizam perfeitamente com o seu prato, seja
             carne ou massa!
           </p>
         </div>
