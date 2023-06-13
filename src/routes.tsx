a20ae3c119c6758f6ca94d9667ba5a8093064ee5
carmencmattos <carmencmattos@gmail.com>
2023-06-13 19:29:30 -0300

    feature: config navigate

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,10 +6,11 @@ import PaginaPadrao from 'components/PaginaPadrao';
 import Sobre from 'pages/Sobre';
 import Footer from 'components/Footer';
 import NotFound from 'pages/NotFound';
+import Prato from 'pages/Prato';
 
 export default function AppRouter() {
   return (
-    <main className='container'>
+    <main className="container">
       <Router>
         <Menu />
         <Routes>
@@ -18,6 +19,7 @@ export default function AppRouter() {
             <Route path="cardapio" element={<Cardapio />} />
             <Route path="sobre" element={<Sobre />} />
           </Route>
+          <Route path="prato/:id" element={<Prato />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         <Footer />
